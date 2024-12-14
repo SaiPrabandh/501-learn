ba58a571f88663cbf03027718e225ee8fd434a6c
yspe <saiprabandh2004@gmail.com>
2024-12-14 23:27:20 +0530

    second commit

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-function hello(){
-    console.log("Hello");
-}
-hello()
+const http=require("http");
+const fs=require('fs')
+
+const server=http.createServer((req,res)=>{
+    fs.readFile("sample.txt",(err,data)=>{
+        res.end(data);
+    });
+});
+server.listen(3000);
