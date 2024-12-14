33405bdd300f24cc5aeb5538e4a45802fe5c1010
yspe <saiprabandh2004@gmail.com>
2024-12-14 23:49:10 +0530

    4th commit

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
 const http=require("http");
-const fs=require('fs')
+const fs=require('fs');
 
 const server=http.createServer((req,res)=>{
-    fs.readFile("sample.txt",(err,data)=>{
-        res.end(data);
-    });
+    const stream=fs.createReadStream("sample.txt");
+    stream.pipe(res);
 });
 server.listen(3000);
